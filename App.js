b4011f98a50b5596394859455a314ebcc3e77fee
MikkeGF <n9pemi00students.oamk.fi>
2020-11-25 00:13:27 +0200

    cors

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const mongoose = require('mongoose');
-const cors = require('cors')
+
 require('dotenv/config');
 
 

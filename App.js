27dd383ec62a5582c4339c20b5794bd1a2401bd5
MikkeGF <n9pemi00students.oamk.fi>
2020-12-10 15:10:14 +0200

    ssl kokeiluja

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ const app = express();
 const mongoose = require('mongoose');
 const https = require('https');
 const fs = require('fs');
-
+const http = require('http');
 require('dotenv/config');
 
 
@@ -15,43 +15,63 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const postsRoute = require('./routes/posts');
 
-app.use('/posts',postsRoute)
+app.use('/posts', postsRoute)
 
 
 // connect to DB
 
 const uri = process.env.ATLAS_URI;
-const port  = process.env.NODE_PORT
+const port = process.env.NODE_PORT
 mongoose.connect(uri,
-{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true
+    {
+        useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true
 
-})
+    })
 
-// .then(() => {
-//     console.log('MongoDB connected')
-// })
-
-// .catch(err => console.log(err))
-
-
-
-
-
-
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.send('Working')
 });
 
-app.post('/post',(req,res) => {
+app.post('/post', (req, res) => {
     res.send('Working post')
 });
 
-let sslOptions = {
-    key: fs.readFileSync('./key.pem'),
-    cert: fs.readFileSync('./cert.pem'),
-    passphrase: process.env.PASS
-};
 
-let serverHttps = https.createServer(sslOptions, app).listen(port)
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer({
+    key: fs.readFileSync('/etc/letsencrypt/live/mikkegf.me-0001/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/mikkegf.me-0001/fullchain.pem'),
+}, app);
+
+
+
+
+httpServer.listen(80, () => {
+    console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
+});
+
+
+
+
+
+
+
+
+
+
+
+// let sslOptions = {
+//     key: fs.readFileSync('./key.pem'),
+//     cert: fs.readFileSync('./cert.pem'),
+//     passphrase: process.env.PASS
+// };
+
+app.listen(port, () => {
+    console.log(`Server running at: http://localhost:${port}/`);
+});
 
 
